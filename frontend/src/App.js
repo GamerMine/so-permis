10abd10484898a7766ecbe0d718a824624938f46
sm211563 <maxime-sav@outlook.fr>
2023-12-11 17:46:58 +0100

    Frontend
    - J'ai remis le magnifique header de Léo =)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import './App.css';
 import Test from "./pages/Test";
+import Header from "./components/Header"
 import {ChakraProvider} from "@chakra-ui/react";
 
 function App() {
   return (
       <ChakraProvider>
+          <Header/>
           <Router>
             <div className="container">
               <Routes>
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,57 @@
+const Header = () => {
+
+    const style = {
+        nav: {
+            width: "100%",
+            backgroundColor: "black"
+        },
+
+        header: {
+            backgroundColor: "#0F1411",
+            color: "white",
+            height: "5%",
+            margin: "-8px"
+        },
+
+        imgLogo: {
+            height: "auto",
+            width: "50%"
+        },
+
+        navLink: {
+            color: "white",
+            textDecoration: "none"
+        },
+
+        gridElement: {
+            padding: "20px",
+            objectFit: "contain",
+            fontSize: "30px",
+            textAlign: "center"
+        },
+
+        gridContainer: {
+            display: "grid",
+            gridTemplateColumns: "auto auto auto auto auto auto"
+        }
+    }
+
+    return (
+        <header>
+            <nav style={style.nav}>
+                <div style={style.gridContainer}>
+                    <img style={{...style.imgLogo, ...style.gridElement}}
+                         src="https://www.easysysteme.fr/photos/auto-ecoles/bureaux/so-permis_logo_64f5d2aa4bc5d.png"/>
+
+                    <a style={{...style.gridElement, ...style.navLink}} href="#">Accueil</a>
+                    <a style={{...style.gridElement, ...style.navLink}} href="#">Permis de conduire</a>
+                    <a style={{...style.gridElement, ...style.navLink}} href="#">Code de la route</a>
+                    <a style={{...style.gridElement, ...style.navLink}} href="#">Qui sommes-nous ?</a>
+                    <a style={{...style.gridElement, ...style.navLink}} href="#">Contact</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
