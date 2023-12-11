6bd2b119aa9e9781f4affaf7f246d88461ef566e
LeoSarry <sarry.leo@orange.fr>
2023-12-11 16:23:21 +0100

    header le plus beau du monde

--- app/Views/Commun/Header.php
+++ app/Views/Commun/Header.php
@@ -7,24 +7,14 @@
     <body>
     <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <img id="img-logo" src="https://www.easysysteme.fr/photos/auto-ecoles/bureaux/so-permis_logo_64f5d2aa4bc5d.png"/>
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-            <a class="nav-link" href="#">Accueil</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Permis de conduire</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Code de la route</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Qui sommes-nous ?</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-            </li>
+        <div class="grid-container">
+            <img class="grid-element" id="img-logo" src="https://www.easysysteme.fr/photos/auto-ecoles/bureaux/so-permis_logo_64f5d2aa4bc5d.png"/>
+
+            <a class="grid-element nav-link" href="#">Accueil</a>
+            <a class="grid-element nav-link" href="#">Permis de conduire</a>
+            <a class="grid-element nav-link" href="#">Code de la route</a>
+            <a class="grid-element nav-link" href="#">Qui sommes-nous ?</a>
+            <a class="grid-element nav-link" href="#">Contact</a>
         </ul>
         </div>
     </nav>
