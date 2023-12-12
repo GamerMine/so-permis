f70bf7f64f5151c5786ced8250ec4b9b420c85ba
AsteriLykos <carolinegrenet@laposte.net>
2023-12-12 13:33:29 +0100

    Récupération des avis avec le widget elfsight Faire la commande npm install react-elfsight-widget

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Test from "./pages/Test";
+import Home from "./pages/Home.js";
 import Header from "./components/Header"
 import {ChakraProvider} from "@chakra-ui/react";
 
@@ -10,7 +10,7 @@ function App() {
           <Router>
             <div className="container">
               <Routes>
-                <Route exact path="/" element={<Test />} />
+                  <Route exact path="/" element={<Home />} />
               </Routes>
             </div>
           </Router>
