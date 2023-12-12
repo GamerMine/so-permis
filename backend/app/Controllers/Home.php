045f9666084508fc7062ae6c52fc997f69329091
LeoSarry <sarry.leo@orange.fr>
2023-12-12 16:03:27 +0100

    Ajout de l'update et du get unique + nouveaux élément dans la base

--- backend/app/Controllers/Home.php
+++ backend/app/Controllers/Home.php
@@ -12,16 +12,34 @@ class Home extends BaseController
     }
 
     public function bado() : string 
+
     {
         try {
             require (APPPATH . "Database/DB.inc.php");
+            $retour = ' ';
             // Exécuter le script SQL avec la méthode $this->query()
             $db = DB::getInstance();
             $actualites = $db->getActualites();
             foreach ($actualites as $row) 
             {
-                return ''. $row->getTitreActualite();
+                $retour .= $row->getTitreActualite().' - ';
             }
+            $newsletter = $db->getNewsletters();
+            foreach ($newsletter as $row) 
+            {
+                $retour .= $row->getEmail().' - ';
+            }
+            $formations = $db->getFormations();
+            foreach ($formations as $row) 
+            {
+                $retour .= $row->getNom().' - ';
+            }
+            $administrateurs = $db->getAdministrateurs();
+            foreach ($administrateurs as $row) 
+            {
+                $retour .= $row->getEmail() .' - ';
+            }
+            return $retour;
         } catch (\Throwable $th) {
             return $th;
         }
