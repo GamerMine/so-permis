5df6f7c5093e6b7f81b519b5bc3c829e88cb25f0
Anaelle Mimifir <anaellemimi27@gmail.com>
2023-12-12 16:33:28 +0100

    ajout de la page Code de la route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home.js";
+import CodeDeLaRoute from "./pages/CodeDeLaRoute.js";
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import {ChakraProvider} from "@chakra-ui/react";
@@ -12,6 +13,7 @@ function App() {
             <div className="container">
               <Routes>
                   <Route exact path="/" element={<Home />} />
+                  <Route exact path="/CodeDeLaRoute" element={<CodeDeLaRoute />} />
               </Routes>
             </div>
           </Router>
