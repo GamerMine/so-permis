b3af3a8885d8cad991b979848d045daf28f5d68b
Anaelle Mimifir <anaellemimi27@gmail.com>
2023-12-12 13:24:14 +0100

    Component Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -29,44 +29,42 @@ const Footer = () => {
     }
 
     return (
-        <footer style={{...style.footer}}>
-        
-        
-        <Stack direction={['column', 'row']} spacing={"5%"}  align='center'>
-            <Box w='30%' align='center'>
-            <img style={{...style.imgLogo}}
-                src="https://www.easysysteme.fr/photos/auto-ecoles/bureaux/so-permis_logo_64f5d2aa4bc5d.png"/>    
-            </Box>
-            <Box>
-                <VStack>
-                    <Box>
-                        <img src="./Insta.png" alt="Instagram"/>
-                        <img src="./Snapchat.png" alt="Snapchat"/>
-                    </Box>
+        <footer style={{...style.footer}}> 
+            <Stack direction={['column', 'row']} spacing={"5%"}  align='center'>
+                <Box w='30%' align='center'>
+                <img style={{...style.imgLogo}}
+                    src="https://www.easysysteme.fr/photos/auto-ecoles/bureaux/so-permis_logo_64f5d2aa4bc5d.png"/>    
+                </Box>
+                <Box>
+                    <VStack>
+                        <Box>
+                            <img src="./Insta.png" alt="Instagram"/>
+                            <img src="./Snapchat.png" alt="Snapchat"/>
+                        </Box>
 
-                    <Box>
-                        <a href="#"><Button style={{...style.bouton}} variant='solid'> Contactez-nous </Button></a>
-                    </Box>
+                        <Box>
+                            <a href="#"><Button style={{...style.bouton}} variant='solid'> Contactez-nous </Button></a>
+                        </Box>
 
-                </VStack>
-            </Box>
-            <Box align='center'>
-                <Text fontSize='2xl' as='u'>Informations</Text>
-                <VStack>
-                    <Link>Accueil</Link>
-                    <Link>Permis de conduire</Link>
-                    <Link>Code de la route</Link>
-                    <Link>Qui sommes-nous ?</Link>
-                </VStack>
-            </Box>
-            <Box align='center'>
-                <Text  fontSize='2xl' as='u'>Coordonnées</Text>
-               
-                <p>20 Rue Jean Lurçat, <br/> 76610 Le Havre</p>
-                <p>02 78 34 10 63</p>
-                <p>sopermis76@gmail.com</p>
-            </Box>
-        </Stack>
+                    </VStack>
+                </Box>
+                <Box align='center'>
+                    <Text fontSize='2xl' as='u'>Informations</Text>
+                    <VStack>
+                        <Link>Accueil</Link>
+                        <Link>Permis de conduire</Link>
+                        <Link>Code de la route</Link>
+                        <Link>Qui sommes-nous ?</Link>
+                    </VStack>
+                </Box>
+                <Box align='center'>
+                    <Text  fontSize='2xl' as='u'>Coordonnées</Text>
+                
+                    <p>20 Rue Jean Lurçat, <br/> 76610 Le Havre</p>
+                    <p>02 78 34 10 63</p>
+                    <p>sopermis76@gmail.com</p>
+                </Box>
+            </Stack>
         </footer>
     )
 }
