263df4914e57c7e9cc1e62efa57021f41df5ccc4
AsteriLykos <carolinegrenet@laposte.net>
2023-12-12 15:40:25 +0100

    utilisation des icons de chakra faire la commande npm install react react-dom react-card-flip @chakra-ui/react

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { ElfsightWidget } from 'react-elfsight-widget';
+import FlipCard from '../components/FlipCard';
 
 import axios from "axios";
 import {
@@ -16,15 +17,17 @@ import {
     Image
 } from "@chakra-ui/react";
 import Header from "../components/Header";
+import {render} from "@testing-library/react";
 
 const Home = () => {
 
     return (
         <Stack marginTop="15px">
-                    <ElfsightWidget widgetId="4b32669e-1d41-4c0b-a813-efdeb3498bad" />;
+            <ElfsightWidget widgetId="4b32669e-1d41-4c0b-a813-efdeb3498bad" />;
+            <FlipCard/>
         </Stack>
-
     );
+
 };
 
 export default Home
