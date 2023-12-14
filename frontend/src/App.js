5374d5f45030e0cf93e6a9ce888c7840171c8eb1
Aanaelle <103115892+Aanaelle@users.noreply.github.com>
2023-12-14 10:20:55 +0100

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Connexion from "./pages/Connexion";
+import Home from "./pages/Home"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
