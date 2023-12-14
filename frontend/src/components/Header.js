f818559328218b8750fab14e8cc8bfbb589b7d61
AsteriLykos <carolinegrenet@laposte.net>
2023-12-14 15:38:50 +0100

    reponsive header et smartphone

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
-import {Box, Grid, GridItem} from "@chakra-ui/react";
+import {Button, Flex, Grid, GridItem, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
+import {FaBars} from 'react-icons/fa'
 
 const Header = () => {
+
+
+    const isSmallDevice = window.matchMedia("(max-width: 560px)").matches;
+
 
     const style = {
         nav: {
@@ -20,9 +25,19 @@ const Header = () => {
             width: "600px"
         },
 
-        navLink: {
-            color: "white",
+        navLinkBig: {
+            color:"white",
             textDecoration: "none",
+            display: "grid",
+            alignSelf:"end"
+        },
+
+        navLinkSmall: {
+            color:"black",
+            textDecoration: "none",
+            display: "grid",
+            alignSelf:"end",
+            fontFamily: "Montserrat, sans-serif",
         },
 
         gridElement: {
@@ -34,10 +49,7 @@ const Header = () => {
         },
 
         gridContainer: {
-            display: "grid",
-            gridTemplateColumns: "auto auto auto auto auto auto",
-            justifyContent: "center",
-            alignItems: "center",
+            verticalAlign:"center",
             height: "100%"
         },
 
@@ -49,19 +61,71 @@ const Header = () => {
 
     return (
         <header>
-            <Grid display={{xl:"flex" , lg:"grid"}} backgroundColor="black">
+        {!isSmallDevice ? (
+
+            <Grid display={{xl:"flex" , lg:"grid"}} backgroundColor="black" height={{xl:"100px" , lg:"213px" ,"sd":"255px" , base:"455px"}}  >
                 <GridItem><img style={style.imgLogo}
                     src="https://www.easysysteme.fr/photos/auto-ecoles/bureaux/so-permis_logo_64f5d2aa4bc5d.png"/></GridItem>
-                <nav style={style.nav}>
-                    <div style={style.gridContainer}>
-                        <GridItem><a style={{...style.gridElement, ...style.navLink}} href="/">Accueil</a></GridItem>
-                        <GridItem><a style={{...style.gridElement, ...style.navLink}} href="#">Permis de conduire</a></GridItem>
-                        <GridItem><a style={{...style.gridElement, ...style.navLink}} href="/CodeDeLaRoute">Code de la route</a></GridItem>
-                        <GridItem><a style={{...style.gridElement, ...style.navLink}} href="#">Qui sommes-nous ?</a></GridItem>
-                        <GridItem><a style={{...style.gridElement, ...style.navLink}} href="#">Contact</a></GridItem>
+                <nav style={style.nav} >
+                    <div>
+                        <Grid marginTop={{lg:"15px"}} style={style.gridContainer} justifyContent={{"sd":"center" , md:"end"}} display={{"sd":"grid" , lg:"flex", base:"grid"}} gridTemplateColumns={{"sd":"repeat(3,1fr)"}}>
+                            <GridItem><a style={{...style.gridElement, ...style.navLinkBig}} href="/">Accueil</a></GridItem>
+                            <GridItem><a style={{...style.gridElement, ...style.navLinkBig}} href="#">Permis de conduire</a></GridItem>
+                            <GridItem><a style={{...style.gridElement, ...style.navLinkBig}} href="/CodeDeLaRoute">Code de la route</a></GridItem>
+                            <GridItem><a style={{...style.gridElement, ...style.navLinkBig}} href="#">Qui sommes-nous ?</a></GridItem>
+                            <GridItem><a style={{...style.gridElement, ...style.navLinkBig}} href="#">Contact</a></GridItem>
+                        </Grid>
                     </div>
                 </nav>
-        </Grid>
+                </Grid>
+    ) :
+        (
+                <Grid display="grid" backgroundColor="black">
+                    <GridItem><img style={style.imgLogo}
+                                   src="https://www.easysysteme.fr/photos/auto-ecoles/bureaux/so-permis_logo_64f5d2aa4bc5d.png"/></GridItem>
+                        <div>
+                            <Menu placement="bottom">
+                                <MenuButton as={Button} color="black" width="80%" marginTop="10px" marginBottom="10px" marginLeft="10%">
+                                    <Grid templateColumns="auto 1fr" align="center">
+                                        <GridItem>
+                                            <FaBars />
+                                        </GridItem>
+                                        <GridItem>
+                                            <text style={style.navLinkSmall} > Menu </text>
+                                        </GridItem>
+                                    </Grid>
+                                </MenuButton>
+                                <MenuList>
+                                    <a style={style.navLinkSmall} href="/">
+                                        <MenuItem>
+                                            Accueil
+                                        </MenuItem>
+                                    </a>
+                                    <a style={style.navLinkSmall} href="#">
+                                        <MenuItem>
+                                            Permis de conduire
+                                        </MenuItem>
+                                    </a>
+                                    <a style={style.navLinkSmall} href="/CodeDeLaRoute">
+                                        <MenuItem>
+                                            Code de la route
+                                        </MenuItem>
+                                    </a>
+                                    <a style={style.navLinkSmall} href="#">
+                                        <MenuItem>
+                                            Qui sommes-nous ?
+                                        </MenuItem>
+                                    </a>
+                                <a style={style.navLinkSmall} href="#">
+                                    <MenuItem>
+                                        Contact
+                                    </MenuItem>
+                                </a>
+                            </MenuList>
+                        </Menu>
+                        </div>
+                </Grid>
+            )}
             <div style={style.bandeBleu}/>
         </header>
     )
