a3859fa0138c78f8e8bb4c6f5eb8762301ed3a56
AsteriLykos <carolinegrenet@laposte.net>
2023-12-15 10:34:46 +0100

    changement taille card version telephone

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -27,16 +27,27 @@ const Home = () => {
         },
     }
 
+    const isSmallDevice = window.matchMedia("(max-width: 449px)").matches;
+
     return (
         <Stack style={{gap: 0}} >
             <Stack style={{backgroundImage: "url('./images/auto-ecole 1.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
 
-            <MultiHorizontalCardsWithButton style={style.cardsServices} cards={
-                [
-                    new OCard("", "PERMIS DE CONDUIRE", "Nos forfaits permis de conduire","permis", "/Permis"),
-                    new OCard("", "CODE", "Nos forfaits code","code", "/CodeDeLaRoute"),
-                    new OCard("", "CONDUITE ACCOMPAGNÉE", "Nos forfaits conduite accompagnée","conduite", "/CodeDeLaRoute"),
-                ]} hauteur={"450px"} largeur={"350px"}/>
+                {!isSmallDevice ? (
+                    <MultiHorizontalCardsWithButton style={style.cardsServices} cards={
+                        [
+                            new OCard("", "PERMIS DE CONDUIRE", "Nos forfaits permis de conduire","permis", "/Permis"),
+                            new OCard("", "CODE", "Nos forfaits code","code", "/CodeDeLaRoute"),
+                            new OCard("", "CONDUITE ACCOMPAGNÉE", "Nos forfaits conduite accompagnée","conduite", "/CodeDeLaRoute"),
+                        ]} hauteur={"450px"} largeur={"350px"}/>
+                ) : (
+                    <MultiHorizontalCardsWithButton style={style.cardsServices} cards={
+                        [
+                            new OCard("", "PERMIS DE CONDUIRE", "Nos forfaits permis de conduire","permis", "/Permis"),
+                            new OCard("", "CODE", "Nos forfaits code","code", "/CodeDeLaRoute"),
+                            new OCard("", "CONDUITE ACCOMPAGNÉE", "Nos forfaits conduite accompagnée","conduite", "/CodeDeLaRoute"),
+                        ]} hauteur={"300px"} largeur={"275px"}/>
+                )}
 
                 <DocumentsInformations titre={"Comment s’inscrire chez So’Permis ?"}/>
             </Stack>
