12a07f28415050c29bfff60a8aa0c51a1775d383
LeoSarry <sarry.leo@orange.fr>
2023-12-18 14:56:57 +0100

    fix bug axios part 2

--- frontend/src/pages/AjouterForfait.js
+++ frontend/src/pages/AjouterForfait.js
@@ -13,7 +13,6 @@ import {
 import { useState, useEffect } from 'react';
 import axios from "axios";
 import Cookies from 'js-cookie';
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const AjouterForfait = () => {
