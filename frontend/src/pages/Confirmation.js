5923258988e50c5858163c656c1188e221f65590
malo <77111396+malo-perso@users.noreply.github.com>
2023-12-20 15:28:46 +0100

    confirmation subscribe newsletter

---
+++ frontend/src/pages/Confirmation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Stack } from "@chakra-ui/react";
+import PannelConfirmation from "../components/PannelConfirmation";
+
+const Confirmation = () => {
+
+    return (
+        <Stack marginTop="15px" gap="0">
+            <PannelConfirmation />
+        </Stack>
+    );
+};
+
+export default Confirmation
