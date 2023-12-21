cc9a00e687282292be02d95297867b93f42156b7
sm211563 <maxime-sav@outlook.fr>
2023-12-21 10:48:18 +0100

    PageAdmin
    - Fix cards

--- frontend/src/pages/PageAdmin.js
+++ frontend/src/pages/PageAdmin.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
 import {HOSTNAME} from "../Variables";
+import PageAdminComponent from "../components/PageAdminComponent";
 
 const PageAdmin = () => {
     const navigate = useNavigate();
@@ -26,40 +27,9 @@ const PageAdmin = () => {
             navigate("/");
         } else {
             setContent((
-                <Stack style={{backgroundImage: "url('./images/code_article_ligne-de-rives.jpeg')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-
-                    <Text style={style.title} textAlign={{base:"center", "sd":"left"}}>DASHBOARD</Text>
-
-                    <MultiHorizontalCardsWithButton style={style.cardsServices} cards={
-                        [
-                            new OCard("", "AJOUTER UN ARTICLE", "Ajouter un article","", "/AjouterArticle"),
-                            new OCard("", "GESTION DES ARTICLES", "Gestion des Articles","", "/GestionArticles"),
-                            new OCard("", "AJOUTER UN FORFAIT", "Ajouter un Forfait ","", "/AjouterForfaits"),
-                            new OCard("", "GESTION DES FORFAITS", "Nos forfaits code","", "/GestionForfaits"),
-                            new OCard("", "CREATION D'UN COMPTE ", "Création d'un Compte","", "/CreationCompte"),
-                        ]} />
-
-                </Stack>
+                <PageAdminComponent/>
             ));
         }
-    }
-
-    const style = {
-        cardsServices: {
-            margin: "50"
-        },
-
-        title:{
-            margin:"50px",
-            color: "black",
-            fontFamily: "Montserrat-Bold, Helvetica",
-            fontSize: "45px",
-            wordWrap: 'break-word',
-            fontStyle: "normal",
-            fontWeight: "700",
-            lineHeight: "normal",
-            textAlign:"center"
-        },
     }
 
     useEffect(() => {
